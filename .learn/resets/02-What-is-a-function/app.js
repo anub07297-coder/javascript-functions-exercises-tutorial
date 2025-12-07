e6017283c97bebae1aaa522df82d193f284d41db
Anee <bisettyanupama@gmail.com>
2025-12-07 18:35:09 -0500

    JavaScript Functions Exercises Tutorial are done

--- .learn/resets/02-What-is-a-function/app.js
+++ .learn/resets/02-What-is-a-function/app.js
@@ -2,8 +2,4 @@ function sum(number1,number2) {
     return number1 + number2;
 }
 
-
 // Your code here
-
-let superduper = sum(3445324,53454423);
-console.log(superduper);
--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,6 +1,33 @@
-// Your code goes here
+// // Your code goes here
+// const rapid = (givenStr)=>{
+// let result ="";
+//    for(let i=0; i<givenStr.length;i++)
+//    {
+//      if( ("aeiou").includes(givenStr[i].toLowerCase())) {         
+//          continue;
+//      }
+//    result += givenStr[i].toUpperCase();
+//    }
+//    return result;
+// };
 
+// // Work above this line; do not change code below
+// let str = "John";
+// console.log(rapid(str));
+
+// Your code goes here
+const rapid = (givenStr)=>{
+let result = givenStr.toLowerCase()
+                     .split("")
+                     .filter((char)=>!("aeiou").includes(char))
+                     .map((element)=> element.toUpperCase())
+                     .join("");
+  
+   return result;
+};
 
 // Work above this line; do not change code below
 let str = "John";
 console.log(rapid(str));
+
+
